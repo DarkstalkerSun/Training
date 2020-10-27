e8ac8be787a7131a44d0f3273bd2bd238458f9d1
Sasha <aferovsky@gmail.com>
2020-10-27 17:28:34 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Training
+{
+    class Motorcycle
+    {
+        public static int driverIntensity;
+        public string driverName;
+        public Motorcycle(int intensity = 0, string name = "")
+        {
+            if (intensity > 10) { intensity = 10; };
+            driverIntensity = intensity;
+            driverName = name;
+        }
+        
+
+
+    }
+}
