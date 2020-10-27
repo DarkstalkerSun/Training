5b8eda0f49ba41e030968eb5b7f1a4a1ddb0663f
Aliaksandr Afiarouski <61987149+DarkstalkerSun@users.noreply.github.com>
2020-10-27 17:54:46 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace Training
         static void Main(string[] args)
         {
             // Code was added in github!
+            Console.WriteLine("The code was added in GitHub");
+            Console.ReadLine();
         } 
 
     }
